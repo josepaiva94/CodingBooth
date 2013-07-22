506918b84ed6de2aefb8d950768ad4d4dbabbd60
José Paiva <josepaiva94@gmail.com>
2013-07-22 16:40:41 +0100

    4

---
+++ templates/cbTheme/index.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @version 1.0.0
+ * @package templates
+ * @subpackage coding booth
+ * @copyright (C) 2013 Coding Booth. All rights reserved.
+ * @author carlospaiva
+ * @license GNU General Public License version 2 or later;
+ */
+defined('_JEXEC') or die;
+
+// Getting params from template
+$params = JFactory::getApplication()->getTemplate(true)->params;
+
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$this->direction = $doc->direction;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->getCfg('sitename');
+
+if($task == "edit" || $layout == "form" )
+{
+	$fullWidth = 1;
+}
+else
+{
+	$fullWidth = 0;
+}
+
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+
+// Add Stylesheets
+$doc->addStyleSheet('templates/'.$this->template.'/css/styles.css');
+
+// Load optional RTL Bootstrap CSS
+JHtml::_('bootstrap.loadCss', false, $this->direction);
+
+// Add current user information
+$user = JFactory::getUser();
+
+// Logo file or site title param
+if ($this->params->get('logo'))
+{
+	$logo = '<img src="'. JURI::root() . $this->params->get('logo') .'" class="logo" alt="'. $sitename .'" />';
+}
+elseif ($this->params->get('sitetitle'))
+{
+	$logo = '<span class="site-title" title="'. $sitename .'">'. htmlspecialchars($this->params->get('sitetitle')) .'</span>';
+}
+else
+{
+	$logo = '<span class="site-title" title="'. $sitename .'">'. $sitename .'</span>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+	<?php
+	// Use of Google Font
+	if ($this->params->get('googleFont'))
+	{
+	?>
+		<link href='http://fonts.googleapis.com/css?family=<?php echo $this->params->get('googleFontName');?>' rel='stylesheet' type='text/css' />
+		<style type="text/css">
+			h1,h2,h3,h4,h5,h6,.site-title{
+				font-family: '<?php echo str_replace('+', ' ', $this->params->get('googleFontName'));?>', sans-serif;
+			}
+		</style>
+	<?php
+	}
+	?>
+	<!--[if lt IE 9]>
+		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body class="site <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+	. ($params->get('fluidContainer') ? ' fluid' : '');
+?>">
+
+	<!-- Body -->
+	<div class="body">
+		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
+			<!-- Header -->
+			<div class="header">
+				<a href="<?php echo $this->baseurl; ?>">
+					<?php echo $logo;?>
+				</a>
+				<div>
+					<jdoc:include type="modules" name="menus" style="none" />
+				</div>
+			</div>
+			<jdoc:include type="modules" name="banner" style="xhtml" />
+			<div class="row-fluid">
+				<?php if ($this->countModules('position-8')) : ?>
+				<!-- Begin Sidebar -->
+				<div id="sidebar" class="span3">
+					<div class="sidebar-nav">
+						<jdoc:include type="modules" name="position-8" style="xhtml" />
+					</div>
+				</div>
+				<!-- End Sidebar -->
+				<?php endif; ?>
+				<div id="content" class="span9">
+					<!-- Begin Content -->
+					<jdoc:include type="modules" name="position-3" style="xhtml" />
+					<jdoc:include type="message" />
+					<jdoc:include type="component" />
+					<jdoc:include type="modules" name="position-2" style="none" />
+					<!-- End Content -->
+				</div>
+				<?php if ($this->countModules('position-7')) : ?>
+				<div id="aside" class="span3">
+					<!-- Begin Right Sidebar -->
+					<jdoc:include type="modules" name="position-7" style="well" />
+					<!-- End Right Sidebar -->
+				</div>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	<!-- Footer -->
+	<div class="footer">
+		<?php if ($this->countModules('footer')) : ?>
+			<div class="container-fluid">
+				<div class="span6">
+					<!-- Begin Right Sidebar -->
+					<jdoc:include type="modules" name="footer" style="well" />
+					<!-- End Right Sidebar -->
+				</div>
+				<div class="span6">
+					<p>&copy; <?php echo date('Y');?> <?php echo $sitename; ?> - All Rights Reserved - Design by <a href="graphicboost.org" target="_blank">Graphic Boost</a></p>
+				</div>
+			</div>
+		<?php else : ?>
+			<div class="container-fluid">
+				<div class="span12">
+					<p>&copy; <?php echo date('Y');?> <?php echo $sitename; ?> - All Rights Reserved - Design by <a href="http://graphicboost.org" target="_blank">Graphic Boost</a></p>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
+	<jdoc:include type="modules" name="debug" style="none" />
+</body>
+</html>
